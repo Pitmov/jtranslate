47991e63a87eb4b8ab139db917988706c552aae0
Андрей Иванычев <pitmov@gmail.com>
2015-05-13 18:59:14 +0400

    Release 0.1

---
+++ README.md
@@ -0,0 +1,68 @@
+# jtranslate 
+
+jtranslate is jquery plugin for translating some text on html page.
+
+
+# Installation
+
+Installation is easy. 
+Include jquery from jquery CDN in header of page:
+
+`<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>`
+
+Include the script jquery.translate.js into footer of your page:
+
+`<script src="/path/to/jquery.translate.js"></script>`
+
+Now, everything installed.
+
+# How to use
+You can translate text on page such as in example:
+
+Basic usage:
+```javascript
+    $(document).ready(function() {
+        $("body").find("a,span,div,p,button,label,select,option,li,h1,h2").addClass("jqt");
+        $('body').jtranslate({
+            lang: "ru",
+            dictionary: {
+                "HOME": {
+                    "ru": "ГЛАВНАЯ"
+                },
+                "SERVICES": {
+                    "ru": "СЕРВИСЫ"
+                },
+                "WORKS": {
+                    "ru": "РАБОТЫ"
+                },
+                "CONTACT": {
+                    "ru": "КОНТАКТЫ"
+                },
+                "DESIGN AGENCY": {
+                    "ru": "АГЕНСТВО ДИЗАЙНА"
+                },
+                "You Think, We Deliver": {
+                    "ru": "Вы Думаете, Мы Создаем"
+                },
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.": {
+                    "ru": "тест тест тест тест тест"
+                },
+                "OUR SERVICES": {
+                    "ru": "НАШИ СЕРВИСЫ"
+                }
+            }
+        });
+
+    });
+```
+
+Also, you can create dictionary.json file to store json information for translations. Then you can load data from this files, for example:
+```javascript
+	$.getJSON("/path/to/dictionary.json", function(json) {
+        var translator = $('body').jtranslate({
+            lang: "ru",
+            dictionary: json
+        });
+```
+
+You can find example in github pages.
---
+++ jquery.translate.js
@@ -0,0 +1,113 @@
+//TODO игнорирование регистра букв в качестве параметра
+//Удаленную загрузку словаря по ссылке
+//Автоматический перевод всего в теге, то есть задаю теги, которые должны быть переведены или задаю один тег и говорю, что все в нем должно быть переведено, автоматом проставляет классы.
+//темплейтный сайт для показа перевода.
+
+(function($) {
+    $.fn.jtranslate = function(options) {
+
+        var self = this, 
+            settings = {
+                classVal: "jqt",
+                lang: "en",
+                dictionary: null
+            },
+        settings = $.extend(settings, options || {});
+
+        this.lang = function(language) {
+            if (language.length) {
+                settings.lang = language;
+                this.jtranslate(settings);
+            }
+
+            return settings.lang;
+        };
+
+        this.get = function(index) {
+            var res = "";
+
+            try {
+                res = settings.dictionary[index][settings.lang];
+            } catch (err) {
+                res = "";
+                return res;
+            }
+
+            if (res.length) {
+                return res;
+            }
+            return index;
+        };
+
+        //search and translate
+        this.find("."+settings.classVal).each(function() {
+            var $this = $(this),
+                key = "",
+                textValue = "",
+                textNodes,
+                textNodeCount = 0,
+                i = 0,
+                trnKey = "",
+                indexArray = [];
+
+            indexArray = $.data($this, "keyСount");
+            if (!indexArray) {
+                indexArray = [];
+                textNodes = $this.currentNodeText();
+                textNodeCount = textNodes.length;
+                for (i = 0; i < textNodeCount; i = i + 1) {
+                    indexArray.push(textNodes[i].index);
+                }
+                $.data($this, "keyCount", indexArray);
+
+                if (textNodeCount > 0) {
+                    for (i = 0; i < textNodeCount; i = i + 1) {
+                        textValue = $.trim($(textNodes[i].textNode).text());
+                        trnKey = textValue.replace('&nbsp;', ' ');
+                        $.data($this, "key_" + indexArray[i], trnKey); //store key for next time
+                    }
+                }
+            }
+            for (i = 0; i < indexArray.length; i = i + 1) {
+                trnKey = $.data($this, "key_" + indexArray[i]);
+                key = self.get(trnKey);
+                if (key !== null) {
+                    $this.setCurrentNodeText(key, indexArray[i]);
+                }
+            }
+        });
+
+        return this;
+    };
+
+    $.fn.currentNodeText = function() {
+        var index = 0,
+            textValue = "",
+            retObject = [],
+            p = $(this.contents().filter(function() {
+                if (this.nodeType == 3) {
+                    textValue = $.trim($(this).text().replace('&nbsp;', ' '));
+                    if (textValue != "" && textValue != null && textValue != "undefined") {
+                        //for ie8
+                        retObject.push({
+                            textNode: this,
+                            index: index
+                        });
+                        //not work in ie8
+                        //this.index = index;
+                        index = index + 1;
+                        return this;
+                    }
+                }
+                index = index + 1;
+            }));
+        return retObject;
+    }
+
+    $.fn.setCurrentNodeText = function(text, index) {
+        var domObject = this.get(0),
+            text_to_change = domObject.childNodes[index];
+
+        text_to_change.nodeValue = text;
+    }
+})(jQuery);
